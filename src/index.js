bd11ef3ede1aff4a3305234658df1271590dd3f2
Jayro Gómez <jaxjayro@gmail.com>
2023-01-15 15:42:02 -0600

    Little changes in port server

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ const app = express()
 
 //Middlewares
 app.use(express.json())
+    //No nested data
 app.use(express.urlencoded({extended: false}))
 
 //Setting the enpoint routes
 const routes = require('./routes/index')
 app.use('/', routes)
 
+const port = process.env.APP_PORT || 3000 
 //API Listening
-app.listen(3000, () => console.log('Server on port 3000'))
+app.listen(port, () => console.log('Server on port 3000'))
