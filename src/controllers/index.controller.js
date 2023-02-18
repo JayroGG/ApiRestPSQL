01390c064deb72d90f268228f7499bbdcee44388
Jayro Gómez <jaxjayro@gmail.com>
2023-02-18 16:12:24 -0600

    Extracting the Db conection logic

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,13 +1,5 @@
-//DB Conection
-const { Pool } = require('pg')
-
-const pool = new Pool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.PORT
-})
+const { Worker } = require('worker_threads')
+const pool = require('../db/dbConection.js')
 
 //Get all the movies function
 const getMovies = async (req, res) => {
---
+++ src/db/dbConection.js
@@ -0,0 +1,12 @@
+//DB Conection
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT
+})
+
+module.exports = pool
