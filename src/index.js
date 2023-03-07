58427fab4620d4c826626e1b7261e95ba78b3b04
Jayro Gómez <jaxjayro@gmail.com>
2023-03-07 11:14:11 -0700

    Align the comments

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 require('dotenv').config()
 
-//Requiring the modules
+// Requiring the modules
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const compression = require('compression')
 const whiteList = [process.env.ORIGIN] || ['http://localhost:3000']
 
-//Filter compression
+// Filter compression
 const filterCompression = (req, res) => {
-  if(req.headers['x-no-compression']){
+  if (req.headers['x-no-compression']) {
     return false
   }
   return compression.filter(req, res)
 }
 
-//Middlewares
+// Middlewares
 app.use(cors({ // Cross Origin
   origin: whiteList,
   allowedHeaders: ['Content-Type'],
@@ -24,16 +24,16 @@ app.use(cors({ // Cross Origin
 app.use(compression({  // Compression above 10 KB
   level: 6,
   threshold: 10 * 1000,
-  filter: filterCompression 
+  filter: filterCompression
 }))
 
-app.use(express.json()) //encoding json
+app.use(express.json()) // Encoding json
 app.use(express.urlencoded({ extended: false })) //No nested data
 
-//Setting the enpoint routes
+// Setting the enpoint routes
 const routes = require('./routes/index')
 app.use('/', routes)
 
-//API Listening
+// API Listening
 const port = process.env.APP_PORT || 3000
 app.listen(port, () => console.log(`Server on Port: ${port}`))
