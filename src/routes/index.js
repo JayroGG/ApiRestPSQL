14a6f696f9f2c17d5f433935fb14a284302fafcc
Jayro Gómez <jaxjayro@gmail.com>
2023-03-07 11:12:02 -0700

    Changed function names to get consintent, re-align comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,33 +1,33 @@
-//Requiring express router module
+// Requiring express router module
 const { Router } = require('express')
 const router = Router()
 
-//Requiring the controller functions
-const { getMovies, getMovieByTitle, postMovie, updateMovie, deleteMovie, notFound, heavyLogic, getMovieById } = require('../controllers/index.controller')
+// Requiring the controller functions
+const { getMovies, getMovieByTitle, createMovie, updateMovieById, deleteMovieById, notFound, heavyLogic, getMovieById } = require('../controllers/index.controller')
 
-//Getting all movies
+// Getting all movies
 router.get('/movies', getMovies)
 
-//Gettin Movies that match the name
+// Gettin Movies that match the name
 router.get('/movies/:title', getMovieByTitle)
 
-//Gettin one Movie by it's ID
+// Gettin one Movie by it's ID
 router.get('/movies/byId/:id', getMovieById)
 
-//Creating a movie register
-router.post('/movies', postMovie)
+// Creating a movie register
+router.post('/movies', createMovie)
 
-//Updatind a movie register
-router.patch('/movies/:id', updateMovie)
+// Updatind a movie register
+router.patch('/movies/:id', updateMovieById)
 
-//Deleting a movie register
-router.delete('/movies/:id', deleteMovie)
+// Deleting a movie register
+router.delete('/movies/:id', deleteMovieById)
 
-//Heavy cost operation
+// Heavy cost operation
 router.get('/heavy', heavyLogic)
 
-//Default 404
+// Default 404
 router.get('*', notFound)
 
-//Exporting
+// Exporting
 module.exports = router
