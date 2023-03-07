c86e905cc9e4da2a709ede256334d8f3a30b1a14
Jayro Gómez <jaxjayro@gmail.com>
2023-03-07 11:15:42 -0700

    Added a condition for empty response, re-align the comments

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,120 +1,110 @@
 const { Worker } = require('worker_threads')
 const pool = require('../db/dbConection.js')
 
-//Get all the movies function
+// Get all the movies function
 const getMovies = async (req, res) => {
     const params = new URLSearchParams(`http://localhost:3000${req.url}`)
     const limit = params.get('limit')
     const offset = params.get('offset')
     try {
-        //Making the SELECT query
+        // Making the SELECT query
         const response = await pool.query('SELECT * FROM movies limit $1 offset $2', [limit, offset])
-
-        //Response
+        // Response
         res.status(200).json({ data: response.rows })
-
     } catch (error) {
         res.status(500).json({ data: [], message: error.message })
     }
-
 }
 
-//Get Movie by Name
+// Get Movie by Name
 const getMovieByTitle = async (req, res) => {
-    //Getting the title
+    // Getting the title
     const title = `%${req.params.title}%`
     const query = 'SELECT * FROM movies WHERE title like $1'
     try {
-        //Making the SELECT query
+        // Making the SELECT query
         const response = await pool.query(query, [title])
-        //Response
-        res.status(200).json({ data: response.rows })
-
+        // Response
+        if (response.rows.length === 0) {
+            res.status(404).json({ data: [], message: 'Movie not found'})
+        } else {
+            res.status(200).json({ data: response.rows })
+        }
     } catch (error) {
         res.status(500).json({ data: [], message: error.message })
     }
 }
 
-//Get Movie by id
+// Get Movie by id
 const getMovieById = async (req, res) => {
-    //Getting the id
+    // Getting the id
     const id = req.params.id
     const query = 'SELECT * FROM movies WHERE id = $1'
     try {
-        //Making the SELECT query
+        // Making the SELECT query
         const response = await pool.query(query, [id])
-        //Response
+        // Response
         res.status(200).json({ data: response.rows })
-
     } catch (error) {
         res.status(500).json({ data: [], message: error.message })
     }
 }
 
-//Creating a new movie register
-const postMovie = async (req, res) => {
-    //Structuring of the body
+// Creating a new movie register
+const createMovie = async (req, res) => {
+    // Destructuring of the body
     const { title, genre, release_date } = req.body
-
-    try {
-        //Insert into postgreSQL table
+     try {
+        // Insert into postgreSQL table
         const response = await pool.query('INSERT INTO movies (title, genre, release_date) VALUES($1, $2, $3)', [title, genre, release_date])
-
-        //Response
+        // Response
         res.status(201).json({
             message: 'Movie Succesfully Added',
             body: {
                 movie: { title, genre, release_date }
             }
         })
-
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 }
 
-//Updating a movie
-const updateMovie = async (req, res) => {
-
-    //Setting the values
+// Updating a movie
+const updateMovieById = async (req, res) => {
+    // Setting the values
     const id = req.params.id
     const { title, genre, release_date } = req.body
-
     try {
-        //Making the UPDATE query
+        // Making the UPDATE query
         const response = await pool.query('UPDATE movies SET title = $1, genre = $2, release_date = $3 WHERE id = $4', [title, genre, release_date, id])
 
-        //Response 
+        // Response 
         res.status(200).json({
             message: 'Movie Succesfully Updated',
             body: {
                 movie: { title, genre, release_date }
             }
         })
-
     } catch (error) {
         res.status(400).json({ message: error.message })
     }
 }
 
-//Deleting a movie register
-const deleteMovie = async (req, res) => {
-    //Getting the id
+// Deleting a movie register
+const deleteMovieById = async (req, res) => {
+    // Getting the id
     const id = req.params.id
-
     try {
-        //Making the DELETE query
+        // Making the DELETE query
         const response = await pool.query('DELETE FROM movies WHERE id = $1', [id])
-
-        //Response
+        // Response
         res.status(200).json(`Movie with id = ${id} succesfully deleted`)
-
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 }
 
-//Not Found 404
+// Not Found 404
 const notFound = async (req, res) => {
     try {
         res.status(404).json({ data: [], message: "Whaat?" })
@@ -123,7 +113,7 @@ const notFound = async (req, res) => {
     }
 }
 
-//Heavy controller
+// Heavy controller
 const heavyLogic = async (req, res) => {
     const countWorker = new Worker('./src/controllers/workers/countWorker.js')
     countWorker.on('message', ({ total, message }) => {
@@ -131,14 +121,14 @@ const heavyLogic = async (req, res) => {
     })
 }
 
-//Exporting the functions
+// Exporting the functions
 module.exports = {
     getMovies,
     getMovieByTitle,
     getMovieById,
-    postMovie,
-    updateMovie,
-    deleteMovie,
+    createMovie,
+    updateMovieById,
+    deleteMovieById,
     notFound,
     heavyLogic
 }
